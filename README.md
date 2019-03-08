55d7f735bf8aefc8d68f14ca57d6343602b69a6d
Nikola Vrhovac <nikola.vrhovac@gowi.rs>
2019-03-08 14:51:56 +0100

    :rocket: initial commit :rocket:

---
+++ README.md
@@ -0,0 +1,23 @@
+# Gulp 4, jQuery, Fontawesome 5 and Bootstrap 4
+
+Use this to startup your project with gulp tasks in a couple of seconds.
+
+### Installation
+
+Requires [node.js](https://nodejs.org/) and [gulp-cli](https://www.npmjs.com/package/gulp-cli) installed globally (-g) to run.
+
+Install the dependencies and devDependencies.
+
+```
+$ cd project
+$ npm install
+$ gulp
+```
+
+### Tech
+
+- [node.js](https://nodejs.org) - Node.js JavaScript runtime
+- [Gulp](https://gulpjs.com/) - Automate and enhance your workflow
+- [Font Awesome](http://fontawesome.io/) - The iconic font and CSS toolkit
+- [Twitter Bootstrap](https://getbootstrap.com/) - Build responsive, mobile-first projects on the web with the world’s most popular front-end component library.
+- [jQuery](https://jquery.com/) - Write less do more library
---
+++ gulpfile.babel.js
@@ -0,0 +1,96 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import concat from 'gulp-concat';
+import sass from 'gulp-sass';
+import del from 'del';
+import uglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
+
+const server = browserSync.create();
+
+const paths = {
+	sctipts: 'src/scripts/*.js',
+	styles: 'src/styles/',
+	dist: 'dist/',
+	dest: 'dist/scripts/',
+	lib: 'src/scripts/',
+	watch: ['src/styles/**', 'src/scripts/**', 'src/*.html']
+};
+
+const clean = () => del(['dist']);
+
+function scripts() {
+	return gulp
+		.src(paths.sctipts, { sourcemaps: true })
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(concat('index.min.js'))
+		.pipe(gulp.dest(paths.dest));
+}
+
+function copy() {
+	return gulp.src(['./src/*.html']).pipe(gulp.dest(paths.dist));
+}
+
+function jsDependencies() {
+	return gulp
+		.src([
+			'node_modules/bootstrap/dist/js/bootstrap.js',
+			'node_modules/jquery/dist/jquery.js',
+			'src/lib/popper.min.js'
+		])
+		.pipe(gulp.dest(paths.dest));
+}
+
+const dependencies = done => {
+	gulp
+		.src(['node_modules/@fortawesome/fontawesome-free/webfonts/**'])
+		.pipe(gulp.dest(`${paths.dist}webfonts/`));
+	done();
+};
+
+const styles = () => {
+	return gulp
+		.src(`${paths.dist}index.scss`, { sourcemaps: true })
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(`${paths.dist}styles/`));
+};
+
+function cssDependencies() {
+	return gulp
+		.src([
+			'node_modules/bootstrap/dist/css/bootstrap.css',
+			'node_modules/@fortawesome/fontawesome-free/css/all.css'
+		])
+		.pipe(gulp.dest(`${paths.dist}styles/`));
+}
+
+function reload(done) {
+	server.reload();
+	done();
+}
+
+function serve(done) {
+	server.init({
+		server: {
+			baseDir: 'dist/'
+		}
+	});
+	done();
+}
+
+const watch = () =>
+	gulp.watch(paths.watch, gulp.series(copy, styles, scripts, reload));
+
+const dev = gulp.series(
+	clean,
+	copy,
+	dependencies,
+	jsDependencies,
+	cssDependencies,
+	styles,
+	scripts,
+	serve,
+	watch
+);
+export default dev;

c93f5a7173c6a16e5ed99ab7050d21ac4d0dfcb7
Nikola Vrhovac <vrllee@gmail.com>
2021-08-28 18:33:20 +0200

    add build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import concat from 'gulp-concat';
 const sass = require('gulp-sass')(require('sass'));
 import del from 'del';
 // import uglify from 'gulp-uglify';
@@ -8,9 +7,14 @@ import browserSync from 'browser-sync';
 import { paths, jsDependencies, cssDependencies } from './gulp.constants';
 
 const server = browserSync.create();
-const clean = () => del(['dist']);
 
-function scripts() {
+/* cleans dist folder */
+function cleanDist() {
+	return del(['dist']);
+}
+
+/* compiles user written javascript */
+function compileCustomJavaScript() {
 	return (
 		gulp
 			.src(paths.scripts, { sourcemaps: true })
@@ -21,39 +25,45 @@ function scripts() {
 	);
 }
 
+/* copies static assets preserving folder structure */
 function copyAssets() {
 	return gulp
 		.src(['./src/assets/**/*'])
 		.pipe(gulp.dest(`${paths.dist}assets/`));
 }
 
-function copy() {
+/* copies all html files from root */
+function copyHtml() {
 	return gulp.src(['./src/*.html']).pipe(gulp.dest(paths.dist));
 }
 
-function js() {
+/* copies third party javascript dependencies */
+function copyJavaScriptDependencies() {
 	return gulp.src(jsDependencies).pipe(gulp.dest(paths.dest));
 }
 
-const fonts = done => {
+/* copies fontawesome font dependencies */
+function copyFontawesomeFonts(done) {
 	gulp
 		.src(['node_modules/@fortawesome/fontawesome-free/webfonts/**'])
 		.pipe(gulp.dest(`${paths.dist}webfonts/`));
 	done();
-};
+}
 
-const styles = () => {
+/* compiles all user styles including bootstrap and other imports */
+function compileSass() {
 	return gulp
 		.src(`${paths.styles}main.scss`, { sourcemaps: true })
 		.pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
 		.pipe(gulp.dest(`${paths.dist}styles/`));
-};
+}
 
-function css() {
+/* copies third party css dependencies */
+function copyCssDependencies() {
 	return gulp.src(cssDependencies).pipe(gulp.dest(`${paths.dist}styles/`));
 }
 
-function reload(done) {
+function reloadServer(done) {
 	server.reload();
 	done();
 }
@@ -67,22 +77,43 @@ function serve(done) {
 	done();
 }
 
-const watch = () =>
+function watch() {
 	gulp.watch(
 		paths.watch,
-		gulp.series(copyAssets, copy, styles, scripts, reload)
+		gulp.series(
+			copyAssets,
+			copyHtml,
+			compileSass,
+			compileCustomJavaScript,
+			reloadServer
+		)
 	);
+}
 
-const dev = gulp.series(
-	clean,
+/* default development gulp command */
+exports.default = gulp.series(
+	cleanDist,
 	copyAssets,
-	copy,
-	fonts,
-	js,
-	css,
-	styles,
-	scripts,
+	copyHtml,
+	copyFontawesomeFonts,
+	copyJavaScriptDependencies,
+	copyCssDependencies,
+	compileSass,
+	compileCustomJavaScript,
 	serve,
 	watch
 );
-export default dev;
+
+/* build for deployment */
+exports.build = gulp.series(
+	cleanDist,
+	gulp.parallel(
+		copyAssets,
+		copyHtml,
+		copyFontawesomeFonts,
+		copyJavaScriptDependencies,
+		copyCssDependencies,
+		compileSass,
+		compileCustomJavaScript
+	)
+);
